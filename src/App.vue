b4f8c3e960e0c2f8240f0f4db10df44d90a12fea
Naoki <lpeopleocomevtogethere@gmail.com>
2022-06-26 13:26:20 +0900

    Working on initial settings

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+  <MainEditor></MainEditor>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
+// import HelloWorld from './components/HelloWorld.vue';
+import MainEditor from './components/MainEditor.vue';
 
 @Options({
   components: {
-    HelloWorld,
+    // HelloWorld,
+    MainEditor
   },
 })
 export default class App extends Vue {}
---
+++ src/components/MainEditor.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="wrapper">
+        test
+    </div>
+    <div class="search-area">
+        this is search area 
+    </div>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style scoped>
+.wrapper {
+    color: red;
+}
+.search-area {
+    background-color: lightblue;
+    height: 35%;
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+}
+</style>
